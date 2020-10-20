809a147823d584a579ea0d64b9181e6f78b514b1
Anton Fedorov <anton.pzkt@gmail.com>
2020-10-20 18:01:04 +0300

    created new middleware for create user id

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,13 @@
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from './middleware/logger';
+import createrId from './middleware/createrId';
 
 import reducer from './reducer';
 
 const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(logger))
+  composeWithDevTools(applyMiddleware(logger, createrId))
 );
 
 export default store;
