cd7e0fabaeeaf14c6c8a6a7b731edfd1faa6cc8a
Anton Fedorov <anton.pzkt@gmail.com>
2020-10-20 18:02:57 +0300

    added new action for add new review

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,6 @@
-import { INCREMENT, DECREMENT, REMOVE } from './constants';
+import { INCREMENT, DECREMENT, REMOVE, ADDUSER } from './constants';
 
 export const increment = (id) => ({ type: INCREMENT, payload: { id } });
 export const decrement = (id) => ({ type: DECREMENT, payload: { id } });
 export const remove = (id) => ({ type: REMOVE, payload: { id } });
+export const addUser = (id) => ({ type: ADDUSER, payload: { id } });
