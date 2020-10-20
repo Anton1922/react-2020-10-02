7dc4a29871fcb6d2a59ad265d954ea1934295569
Anton Fedorov <anton.pzkt@gmail.com>
2020-10-20 17:53:38 +0300

    added reducer 'users'

---
+++ src/redux/reducer/users.js
@@ -0,0 +1,15 @@
+import { normalizedUsers } from '../../fixtures';
+
+const defaultUsers = normalizedUsers.reduce(
+  (acc, user) => ({ ...acc, [user.id]: user }),
+  {}
+);
+console.log('defaultUsers', defaultUsers);
+export default (users = defaultUsers, action) => {
+  const { type } = action;
+
+  switch (type) {
+    default:
+      return users;
+  }
+};
